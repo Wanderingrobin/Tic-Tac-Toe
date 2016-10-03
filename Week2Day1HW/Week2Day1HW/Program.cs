c8a846ea9167acb45cd2da9f5ae1b17355e03260
Wanderingrobin <1stladyofkrypton@gmail.com>
2016-10-03 15:50:26 -0400

    Why won't it work??

---
+++ Week2Day1HW/Week2Day1HW/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.VideoGames
+{
+    class VideoGameSystems
+    {
+        static void Main(string[] args)
+        {
+            VideoGameSystems myNintendo = new VideoGameSystems();
+            
+            //myNintendo.
+            //bool controllerQuestion = myNintendo.HasControllers;
+
+            //bool wifiQuestion = myNintendo.HasWifi;
+
+            //VideoGameSystems mySNES = new VideoGameSystems();
+            //mySNES.HasControllers = true;
+
+        }
+    }
+}
---
+++ Week2Day1HW/Week2Day1HW/VideoGameSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Week2Day1HW
+{
+    class VideoGameSystem
+    {
+        private bool hasControllers = true;
+        private int numControllers;
+        private bool hasWifi;
+        private bool hasCartridges;
+        private bool hasCD;
+        private bool hasDigitalDownload;
+
+        public VideoGameSystem()
+        {
+        }
+
+        public VideoGameSystem(string myNintendo)
+        {
+            //Console.WriteLine(myNintendo);
+        }
+
+        public VideoGameSystem(int initialNumControllers)
+        {
+            numControllers = initialNumControllers;
+        }
+
+        public bool HasControllers
+        {
+            get
+            {
+                return HasControllers;
+            }
+        }
+
+        public int NumControllers
+        {
+            get
+            {
+                return numControllers;
+            }
+
+            set
+            {
+                if (value < 1)
+                {
+                    numControllers = 2;
+                }
+                else
+                {
+                    numControllers = value;
+                }
+            }
+        }
+    }
+}
